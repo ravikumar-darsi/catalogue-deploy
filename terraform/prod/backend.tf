e3555916549f19f072fd038b76c9a19a09fa675d
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-09-20 16:26:16 +0530

    bucket name changed

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,4 +1,4 @@
-bucket         = "daws67s-state-dev-new"
+bucket         = "daws67s-state-prod-new"
 key            = "catalogue"
 region         = "us-east-1"
-dynamodb_table = "daws67s-locking-dev" 
+dynamodb_table = "daws67s-locking-prod"
